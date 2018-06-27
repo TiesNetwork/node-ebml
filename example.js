27e5525e7f5c5a834f298e3fbc6f3c299afb94b6
Dmitry Kochin <dco@mail.ru>
2018-06-27 18:05:11 +0300

    Example and value conversion fixes

--- example.js
+++ example.js
@@ -1,73 +1,183 @@
-var fs = require('fs');
-var util = require('util');
+const EBML = require('./index.js');
+const util = require('util');
 
-var codec = require('./request/codec');
-var Connection = require('./net/connection');
-var uuidv4 = require('uuid/v4');
-var bigdecimal = require("bigdecimal");
-var Record = require("./request/record");
+//Define document schema
+let _the_schema = {
+	"RootTag": {
+        "tag": "1e544945", //Hex tags should be lowercase only
+		"type": "m",
+        "description": "Root tag"
+	},
+	"SubTag": {
+        "context": ["RootTag"], //The tag can be only a direct child of RootTag
+        "tag": "80",
+		"type": "m",
+        "description": "Sub tag"
+	},
+	"StringVal": {
+        "context": ["SubTag", "ValueList"], //The tag can be only a direct child of SubTag or StringList
+        "tag": "80", //The tags can duplicate as long as they can be differentiated by parent contexts
+		"type": "8",
+        "description": "Utf8 string"
+	},
+	"ValueList": {
+        "context": ["SubTag"], //The tag can be only a direct child of SubTag
+        "tag": "81",
+        "type": "m",
+        "description": "String list"
+	},
+	"IntVal": {
+        "context": ["^SubTag"], //The tag can be only a direct or indirect child of SubTag
+        "tag": "a0",
+		"type": "i",
+        "description": "Int value"
+	},
+    "UIntVal": {
+        "context": ["^SubTag"], //The tag can be only a direct or indirect child of SubTag
+        "tag": "a2",
+        "type": "u",
+        "description": "UInt value"
+    },
+    "DateVal": {
+        "context": ["^SubTag"], //The tag can be only a direct or indirect child of SubTag
+        "tag": "a1",
+        "type": "d",
+        "description": "Date value"
+    }
 
-fs.readFile('myreq.proto', async function(err, data) {
-    if (err)
-        throw err;
-    //var req = codec.decode(data, new Buffer("fafe9c9e7845f446d091c12c74d44c61a0923c00", "hex"));
-    //console.log(util.inspect(req, {showHidden: false, depth: null}));
+};
 
-    //data = codec.encode(req);
-    //console.log(data.toString('hex'));
+let schema = new EBML.Schema(_the_schema);
 
-    let types = {
-        Id: 'uuid',
-        fBinary: 'binary',
-        fBoolean: 'boolean',
-        fDecimal: 'decimal',
-        fDouble: 'double',
-        fDuration: 'duration',
-        fFloat: 'float',
-        fInteger: 'integer',
-        fLong: 'long',
-        fString: 'string',
-        fTime: 'time'
-    };
+//Create our document for serializing to EBML
+let doc = {
+    name: "RootTag",
+    children: [
+        {
+            name: "SubTag",
+            children: [
+                {
+                    name: "StringVal",
+                    data: EBML.tools.toBinary("Some string", "8")
+                },
+                {
+                    name: "ValueList",
+                    children: [
+                        {
+                            name: "StringVal",
+                            data: EBML.tools.toBinary("Another string", "8")
+                        },
+                        {
+                            name: "IntVal",
+                            data: EBML.tools.toBinary(42, "i")
+                        },
+                        {
+                            name: "IntVal",
+                            data: EBML.tools.toBinary(-42, "i")
+                        },
+                        {
+                            name: "UIntVal",
+                            data: EBML.tools.toBinary(554, "u")
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            name: "SubTag",
+            children: [
+                {
+                    name: "StringVal",
+                    data: EBML.tools.toBinary("Yet another string", "8")
+                },
+                {
+                    name: "DateVal",
+                    data: EBML.tools.toBinary(new Date("2018-07-27T15:58:00"), "d")
+                },
+                {
+                    name: "UIntVal",
+                    data: EBML.tools.toBinary(-554, "u")
+                },
 
-    let record = new Record('client-dev.test', 'all_types');
-    let uuid = uuidv4();
-    record.putFields({
-        Id: uuid,
-        fBinary: new Buffer("e0a61e5ad74f", 'hex'),
-        fBoolean: false,
-        fDecimal: new bigdecimal.BigDecimal("-1.235e-8"),
-        fDouble: 158.234e200,
-        fDuration: 20*86400,
-        fFloat: -42803.234e-8,
-        fInteger: 423424432,
-        fLong: -278374928374,
-        fString: "This is UTF-8 строка",
-        fTime: new Date()
-    }, types);
+            ]
+        }
+    ]
+};
 
-    //0xe0a61e5ad74fc154927e8412c7f03528134f755e7eb45554eb7a99c2744ac34e
-    //0xAe65bAf610Bad3F0d71Aa3C3a8110c2d62cbEb19
+//Let us encode our doc
+function encode(root, schema) {
+    let encoder = new EBML.Encoder(null, schema);
+    let encodedDoc;
+    encoder.on("data", chunk => {
+        encodedDoc = chunk;
+    });
 
-    let c = new Connection();
-    await c.connect('ws://192.168.1.44:8080/websocket');
+    function encodeInner(node){
+        let info = schema.findTagByName(node.name);
+        if(info.type === 'm'){
+            encoder.write(['start', node]);
+            for(let i=0; i<node.children.length; ++i){
+                encodeInner(node.children[i]);
+            }
+            encoder.write(['end', node]);
+        }else{
+            encoder.write(['tag', node]);
+        }
+    }
 
-    let response = await c.modify([record], Buffer.from('e0a61e5ad74fc154927e8412c7f03528134f755e7eb45554eb7a99c2744ac34e', 'hex'));
+    encodeInner(root);
+    return encodedDoc;
+}
 
-    let selresp = await c.recollect(
-        `SELECT 
-            Id, 
-            bigIntAsBlob(toUnixTimestamp(CAST(writeTime(fTime) AS date))) AS WriteTime, 
-            intAsBlob(0x309) AS TestValue 
-        FROM "client-dev.test"."all_types"
-        WHERE
-            Id IN (${uuid.toString()})`
-    );
+let encodedDoc = encode(doc, schema);
 
-//    response = await c.modify([record], Buffer.from('e0a61e5ad74fc154927e8412c7f03528134f755e7eb45554eb7a99c2744ac34e', 'hex'));
+//Now we have encoded DOC into EBML
+console.log("EBML encoded doc: " + encodedDoc.toString('hex'));
 
-    console.log(util.inspect(selresp, {showHidden: false, depth: null}));
+//Let us now decode it back
+function decode(buffer, schema) {
+    let decoder = new EBML.Decoder(null, schema);
+    let stack = [{children: []}];
 
-    c.close();
-    
-});
+    decoder.on('data', function(chunk) {
+        if(chunk[0] === 'start') {
+            let item = stack[stack.length - 1];
+
+            let data = chunk[1];
+            let item1 = {
+                name: data.name,
+                type: data.type,
+                children: []
+            };
+
+            item.children.push(item1);
+            stack.push(item1);
+        }else if(chunk[0] === 'tag') {
+            let item = stack[stack.length - 1];
+
+            let data = chunk[1];
+            let item1 = {
+                name: data.name,
+                type: data.type,
+                data: data.data,
+                value: data.value
+            };
+
+            item.children.push(item1);
+        }else{ //chunk[0] === 'end'
+            stack.pop();
+        }
+    });
+
+    decoder.write(buffer);
+
+    if(decoder._tag_stack.length)
+        throw new Error('Failed to parse: ' + buffer.toString('hex'));
+
+    return stack[0].children[0];
+}
+
+let new_doc = decode(encodedDoc, schema);
+
+//Now we have decoded original doc
+console.log("Decoded doc: ", util.inspect(new_doc, {showHidden: false, depth: null}));
